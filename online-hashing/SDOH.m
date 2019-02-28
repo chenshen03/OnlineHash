d93306431cc39de7181b0e56905ec33c0b012a85
chenshen03 <675121663@qq.com>
2019-02-28 20:43:22 +0800

    SDOH算法unseen评测程序完善

--- online-hashing/SDOH.m
+++ online-hashing/SDOH.m
@@ -2,53 +2,61 @@ close all; clear; clc;
 sprintf("Loading cifar-zeroshot datasets...");
 opts.dirs.data = '/home/chenshen/Projects/Hash/OnlineHash/data';
 opts.unsupervised = 0;
-opts.nbits = 8;
+opts.nbits = 64;
 normalizeX = 0;
+opts.unseen = 1;
 
-DS = Datasets.cifar_zs(opts, normalizeX);
-trainCNN = DS.Xtrain;
-testCNN = DS.Xtest;
-retrievalCNN = DS.Xretrieval;
-trainLabels = DS.Ytrain;
-testLabels = DS.Ytest;
-retrievalLabels = DS.Yretrieval;
 
-% mapped into a sphere space
+%% 数据处理
+% 根据unseen标志选择对应的数据集
+if opts.unseen == 1
+    DS = Datasets.cifar_zs(opts, normalizeX);
+else
+    DS = Datasets.cifar(opts, normalizeX);
+end
+
 % 训练集
-%train = trainCNN ./ sqrt(sum(trainCNN .* trainCNN, 2));   %  n x d
-train = trainCNN;
-trainLabel = trainLabels; % n x 1
+trainCNN = double(DS.Xtrain);
+trainLabels = DS.Ytrain;
+%train = trainCNN ./ sqrt(sum(trainCNN .* trainCNN, 2));   % mapped into a sphere space
+train = trainCNN';
+trainLabel = trainLabels;
+[Ntrain, Dtrain] = size(trainCNN);
+
 % 测试集
-%test = testCNN ./ sqrt(sum(testCNN .* testCNN, 2));  % n x  d
-test = testCNN;
-testLabel = testLabels;  % n x 1
+testCNN = double(DS.Xtest);
+testLabels = DS.Ytest;
+%test = testCNN ./ sqrt(sum(testCNN .* testCNN, 2));  % mapped into a sphere space
+test = testCNN';
+testLabel = testLabels;
+[Ntest, Dtest] = size(testCNN);
+
 % 检索集
-%retrieval = retrievalCNN ./ sqrt(sum(retrievalCNN .* retrievalCNN, 2));  % n x  d
-retrieval = retrievalCNN;
-retrievalLabel = retrievalLabels;  % n x 1
+if opts.unseen == 1
+    retrievalCNN = double(DS.Xretrieval);
+    retrievalLabels = DS.Yretrieval;
+    %retrieval = retrievalCNN ./ sqrt(sum(retrievalCNN .* retrievalCNN, 2));  % mapped into a sphere space
+    retrieval = retrievalCNN';
+    retrievalLabel = retrievalLabels;
+    [Nretrieval, Dretrieval] = size(retrievalCNN);
+end
 
 clear trainCNN testCNN retrievalCNN trainLabels testLabels retrievalLabels
 
-[Ntrain, Dtrain] = size(train);
-[Ntest, Dtest] = size(test);
-[Nretrieval, Dretrieval] = size(retrieval);
 
+%% 参数初始化
 n_t = 2000;
 alpha = 1e-2;
-sigma = 0.3;  % places 上0.4上下调整, 一般情况下，sigma越大loss越小,反之越大. 
-lr = 0.1;
-threshold = 1e-3;
+sigma = 0.5;  % places 上0.4上下调整, 一般情况下，sigma越大loss越小,反之越大. 
+lr = 0.05;
+threshold = 1e-4;
 pos = 1;
 neg = 1;
 
-train = train';
-test = test';
-retrieval = retrieval';
 W_t = randn(Dtest, opts.nbits);
 W_t = W_t ./ repmat(diag(sqrt(W_t' * W_t))', Dtest, 1);
 training_size = 20000;
 
-ii = 0;
 results = [];
 
 tic;
@@ -69,45 +77,41 @@ for t = 1:n_t:training_size
         P = double(L_t == L_t');
         P = normpdf(P, 1, sigma);
         S = double(L_t == L_t');
-%        P(P == 1) = 0.4;
-%        P(P == 0) = 0.001;
+        % P(P == 1) = 0.4;
+        % P(P == 0) = 0.001;
     end
     [ydata, W_t, obj, Q, P] = minKLD(alpha/n_t, P, opts.nbits, X_t, W_t, S, lr, threshold, pos, neg);
     
-    % ii = ii + 1;
-    % logInfo('iter: %g', ii);
-    % Htrain = single(W_t' * train > 0);
-    % Htest = single(W_t' * test > 0);
-    % Aff = affinity([], [], trainLabel, testLabel, opts);
-    % opts.metric = 'mAP';
-    % res = evaluate(Htrain', Htest', opts, Aff);
-    % results = [results; res];
 end
 toc;
 
-Hretrieval = single(W_t' * retrieval > 0);
+
+%% 评价指标
 Htest = single(W_t' * test > 0);
-Aff = affinity([], [], retrievalLabel, testLabel, opts);
+if opts.unseen == 1
+    Htrain = single(W_t' * retrieval > 0);
+    Aff = affinity([], [], retrievalLabel, testLabel, opts);
+else
+    Htrain = single(W_t' * train > 0);
+    Aff = affinity([], [], trainLabel, testLabel, opts);
+end
+
+opts.metric = 'prec_k1';
+opts.prec_k = 1;
+res = evaluate(Htrain', Htest', opts, Aff);
+
+opts.metric = 'prec_n2';
+opts.prec_n = 2;
+res = evaluate(Htrain', Htest', opts, Aff);
+
 opts.metric = 'mAP';
-res = evaluate(Hretrieval', Htest', opts, Aff);
-results = [results; res];
+res = evaluate(Htrain', Htest', opts, Aff);
 
-disp(results);
-auc   = arrayfun(@(x) mean(x), results);
-final = arrayfun(@(x) x(end) , results);
-logInfo('');
-logInfo('  AUC mAP: %.3g +/- %.3g', mean(auc), std(auc));
-logInfo('FINAL mAP: %.3g +/- %.3g', mean(final), std(final));
-
-% opts.metric = 'mAP';
-% res = evaluate(Htrain', Htest', opts, Aff);
-
-% opts.metric = 'prec_n2';
-% opts.prec_n = 2;
-% res = evaluate(Htrain', Htest', opts, Aff);
-
-% opts.metric = 'prec_k1';
-% opts.prec_k = 1;
-% res = evaluate(Htrain', Htest', opts, Aff);
+% results = [results; res];
+% auc   = arrayfun(@(x) mean(x), results);
+% final = arrayfun(@(x) x(end) , results);
+% logInfo('');
+% logInfo('  AUC mAP: %.3g +/- %.3g', mean(auc), std(auc));
+% logInfo('FINAL mAP: %.3g +/- %.3g', mean(final), std(final));
 
 clear;
