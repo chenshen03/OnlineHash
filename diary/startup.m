8060a7ee8b7d265a7c91fa42fe138db6ac34d1d5
chenshen03 <675121663@qq.com>
2019-03-01 12:33:01 +0800

    三个数据集上的所有运行命令

--- diary/startup.m
+++ diary/startup.m
@@ -20,5 +20,33 @@ demo_online('HCOH','cifar',64,'unseen',1,'ntrials',1,'ntest',2,'numTrain', 20000
 
 demo_online('SDOH','cifar',64,'unseen',1,'ntrials',1,'ntest',2,'numTrain', 20000,'updateInterval',2000,'trigger','fix','reservoirSize',200,'override',1)
 
+% MNIST
+demo_online('OKH','mnist',64,'unseen',1,'ntrials',1,'ntest',2,'numTrain',20000,'updateInterval',100,'trigger','fix','reservoirSize',200,'override',1)
 
+demo_online('SketchHash','mnist',64,'unseen',1,'ntrials',1,'ntest',2,'numTrain',20000,'updateInterval',100,'trigger','fix','reservoirSize',200,'override',1)
+
+demo_online('AdaptHash','mnist',64,'unseen',1,'ntrials',1,'ntest',2,'numTrain', 20000,'updateInterval',100,'trigger','fix','reservoirSize',200,'override',1)
+
+demo_online('OSH','mnist',64,'unseen',1,'ntrials',1,'ntest',2,'numTrain', 20000,'updateInterval',100,'trigger','fix','reservoirSize',200,'override',1)
+	
+demo_online('MIHash','mnist',64,'unseen',1,'ntrials',1,'ntest',2,'numTrain',20000,'updateInterval',100,'trigger','mi','reservoirSize',1000,'override',1)
+
+demo_online('HCOH','mnist',64,'unseen',1,'ntrials',1,'ntest',2,'numTrain', 20000,'updateInterval',100,'trigger','fix','reservoirSize',200,'override',1)
+
+demo_online('SDOH','mnist',64,'unseen',1,'ntrials',1,'ntest',2,'numTrain', 20000,'updateInterval',2000,'trigger','fix','reservoirSize',200,'override',1)
+
+% PLACE
+demo_online('OKH','place',64,'unseen',1,'ntrials',1,'ntest',2,'numTrain',100000,'updateInterval',100,'trigger','fix','reservoirSize',200,'override',1)
+
+demo_online('SketchHash','place',64,'unseen',1,'ntrials',1,'ntest',2,'numTrain',100000,'updateInterval',100,'trigger','fix','reservoirSize',200,'override',1)
+
+demo_online('AdaptHash','place',64,'unseen',1,'ntrials',1,'ntest',2,'numTrain', 100000,'updateInterval',100,'trigger','fix','reservoirSize',200,'override',1)
+
+demo_online('OSH','place',64,'unseen',1,'ntrials',1,'ntest',2,'numTrain', 100000,'updateInterval',100,'trigger','fix','reservoirSize',200,'override',1)
+	
+demo_online('MIHash','place',64,'unseen',1,'ntrials',1,'ntest',2,'numTrain',100000,'updateInterval',100,'trigger','mi','reservoirSize',5000,'override',1)
+
+demo_online('HCOH','place',64,'unseen',1,'ntrials',1,'ntest',2,'numTrain', 100000,'updateInterval',100,'trigger','fix','reservoirSize',200,'override',1)
+
+demo_online('SDOH','place',64,'unseen',1,'ntrials',1,'ntest',2,'numTrain', 100000,'updateInterval',2000,'trigger','fix','reservoirSize',200,'override',1)
 % LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6 matlab
